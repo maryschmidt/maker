1b78c44eee7d8265a544d9218b0767bc95699691
Mary Schmidt <marykateschmidt@gmail.com>
2013-01-08 14:15:23 -0500

    fully nested projects, sessions redirected after login

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,41 +1,41 @@
 class ProjectsController < ApplicationController
   def index
-    @projects = Project.all
+    @projects = current_user.projects.all
   end
 
   def show
-    @project = Project.find(params[:id])
+    @project = current_user.projects.find(params[:id])
   end
 
   def new
-    @project = Project.new
+    @project = current_user.projects.new
   end
 
   def edit
-    @project = Project.find(params[:id])
+    @project = current_user.projects.find(params[:id])
   end
 
   def create
-    @project = Project.new(params[:project])
+    @project = current_user.projects.new(params[:project])
     if @project.save
-      redirect_to @project, notice: 'Project was successfully created.'
+      redirect_to [current_user, @project], notice: 'Project was successfully created.'
     else
       render action: "new"
     end
   end
 
   def update
-    @project = Project.find(params[:id])
+    @project = current_user.projects.find(params[:id])
     if @project.update_attributes(params[:project])
-      redirect_to @project, notice: 'Project was successfully updated.'
+      redirect_to [current_user, @project], notice: 'Project was successfully updated.'
     else
       render action: "edit"
     end
   end
 
   def destroy
-    @project = Project.find(params[:id])
+    @project = current_user.projects.find(params[:id])
     @project.destroy
-    redirect_to projects_url
+    redirect_to @user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Maker::Application.routes.draw do
-  resources :projects
 
-  resources :users, only: [:index, :show]
+  resource  :session,
+    :controller => 'sessions',
+    :only => [:new, :create, :destroy]
+
+  resources :users, only: [:index, :show] do
+    resources :projects
+  end
+
+  match '/sign_out' => 'sessions#destroy', :via => :delete
 
   root :to => 'Clearance::Sessions#new'
 
