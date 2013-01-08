2bd35beab68ffb2502533e0e2b148662226173c8
Mary Schmidt <marykateschmidt@gmail.com>
2013-01-08 16:20:28 -0500

    beginning to add images

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ActiveRecord::Base
   attr_accessible :description, :title
   belongs_to :user
+  attr_accessible :image
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
