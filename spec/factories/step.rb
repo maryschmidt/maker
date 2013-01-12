0765a821c47e6927c149f581729e1ff60e6581f5
Mary Schmidt <marykateschmidt@gmail.com>
2013-01-12 15:59:50 -0500

    added step tests

---
+++ spec/factories/step.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+
+  factory :step do
+    name "name"
+    detail "detail"
+    tip "tip"
+    project
+  end
+
+end
--- spec/models/step_spec.rb
+++ spec/models/step_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 
-describe Project do
-  it "should belong to a user" do
+describe Step do
+  it "should belong to a project" do
     user = create(:user)
     project = create(:project, user: user)
     project.user.should == user
+    step = create(:step, project: project)
+    step.project.should == project
   end
 end
