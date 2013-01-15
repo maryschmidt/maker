acf2ddd0c396284a0e5e708280f54c1f7489e1e8
Mary Schmidt <marykateschmidt@gmail.com>
2013-01-15 16:51:25 -0500

    first pass

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,34 @@ describe User do
     User.authenticate('email@email.com', 'password').should == nil
   end
 
+  it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:unfollow!) }
+
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
+    end
+  end
 end
