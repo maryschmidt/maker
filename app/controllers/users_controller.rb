4e77bb13401e874a8cf57a989af0994437ad9555
Mary Schmidt <marykateschmidt@gmail.com>
2013-01-18 11:27:13 -0500

    added in a mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
-class UsersController < ApplicationController
+class UsersController < Clearance::UsersController
   def index
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def create
+    super
+    if @user.persisted?
+      UserMailer.welcome_email(@user).deliver
+    end
   end
 end
